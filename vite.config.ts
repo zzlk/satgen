b72228471902f1573572d72802371d41b14a8207
zzlk <2418897+zzlk@users.noreply.github.com>
2025-08-30 06:44:24 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: process.env.NODE_ENV === "production" ? "/satgen/" : "/",
   worker: {
     format: "es",
   },
